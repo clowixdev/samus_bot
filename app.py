240646f7889e856dc0349ba09bd1176933e19030
Nikita-LAPTOP <clowixdev@gmail.com>
2024-07-01 16:17:59 +0300

    fixed timeout errors, just restarting the bot

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from loader import bot
+from time import sleep
+from datetime import datetime
+from requests.exceptions import ConnectionError, ReadTimeout
+from sys import exit
 import commands
 
-if __name__ == '__main__':
-    print('Bot script has been successfully enabled')
-    bot.infinity_polling()
+if __name__ == "__main__":
+    while True:
+        try:
+            print(f"Bot script has been successfully enabled at {datetime.now()}")
+            bot.polling(non_stop=True, interval=0)
+        except KeyboardInterrupt:
+            print(f"Shutting down the bot at {datetime.now()}")
+            exit(0)
+        except (ConnectionError, ReadTimeout) as r_e:
+            print(datetime.now(), r_e)
+            sleep(5)
+            continue
