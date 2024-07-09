e94ad818211bf5a7308a4619dcd7cab699e492fe
Nikita-LAPTOP <clowixdev@gmail.com>
2024-07-09 03:55:40 +0300

    removed chat kick

--- commands/kick.py
+++ commands/kick.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from database.msg_templates import REPLIES
 from database.dbworker import get_user, delete_user
 
-from loader import bot, engine, CHATS
+from loader import bot, engine
 
 from functions.funcs import cut_username
 from functions.decorators import group_required, admin_required, spam_checker
@@ -14,7 +14,7 @@ from functions.decorators import group_required, admin_required, spam_checker
 @group_required
 @admin_required
 def kick_command(message: Message)-> None:
-    """Handler that provides work of "/kick" command. User will be kicked from all chats and removed from base
+    """Handler that provides work of "/kick" command. User will be kicked removed from database
 
     Args:
         message (Message): Object, that contains information of received message
@@ -27,9 +27,6 @@ def kick_command(message: Message)-> None:
         bot.reply_to(message, REPLIES["kick_error_msg"])
     else:
         bot.reply_to(message, REPLIES["kick_msg"].format(username=user_to_kick.username, rr_name=user_to_kick.rr_name))
-
-        for chat in CHATS:
-            bot.kick_chat_member(chat, get_user(None, username, engine).id)
     
         delete_user(user_to_kick.id, engine)
 
