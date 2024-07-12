1281ed10f194646694345c036a006c63148db042
Nikita-LAPTOP <clowixdev@gmail.com>
2024-07-12 05:09:24 +0300

    added 'leave' message

--- commands/group.py
+++ commands/group.py
@@ -15,3 +15,15 @@ def welcome_message(message: Message) -> None:
 
     bot.reply_to(message, REPLIES["welcome_message"])
     print("{date} {username} with id {id} entered our group chat in {chat_id}".format(date=datetime.now(), username=message.from_user.username, id=message.from_user.id, chat_id=message.chat.id))
+
+
+@bot.message_handler(content_types=["left_chat_members"])
+def leaving_message(message: Message) -> None:
+    """Handler that will force bot to send leaving message to new user of a chat
+
+    Args:
+        message (Message): Object, that contains information of received message
+    """
+
+    bot.reply_to(message, REPLIES["leave_message"])
+    print("{date} {username} with id {id} left our group chat in {chat_id}".format(date=datetime.now(), username=message.from_user.username, id=message.from_user.id, chat_id=message.chat.id))
