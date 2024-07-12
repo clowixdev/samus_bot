a56959e4408f0d8fb138b993b87c84b17efeef22
Nikita-LAPTOP <clowixdev@gmail.com>
2024-07-12 04:28:06 +0300

    added statistic for chat

--- commands/admin.py
+++ commands/admin.py
@@ -1,17 +1,15 @@
 from telebot.types import Message
 from datetime import datetime
 
-from database.msg_templates import REPLIES
 from database.dbworker import gen_users, delete_user
 
-from loader import bot, engine
+from loader import bot, engine, main_chat_id
 from functions.decorators import chat_required, dev_required, spam_checker
 
 
 @bot.message_handler(commands=["clear"])
 @spam_checker
 @dev_required
-@chat_required
 def clear_command(message: Message)-> None:
     """Handler that provides work for "clear" function that will clear database from None username profiles
 
@@ -34,7 +32,6 @@ def clear_command(message: Message)-> None:
 @bot.message_handler(commands=["stats"])
 @spam_checker
 @dev_required
-@chat_required
 def stats_command(message: Message)-> None:
     """Handler that will provide work for stats command that will give me brief info about clan
 
@@ -43,10 +40,10 @@ def stats_command(message: Message)-> None:
     """
 
     all_users = gen_users(engine)
-    for id, user in enumerate(user, 1):
-        print(f"DEV: {id}) {user.username} - {user.rr_name} - {user.crit_dmg}%")
+    for id, user in enumerate(all_users, 1):
+        print(f"DEV: {id}) @{user.username} - {user.rr_name} - {user.crit_dmg}%")
 
-    print(f"DEV: total {len(all_users)} users registered")
+    print(f"DEV: total {len(all_users)} users registered and {bot.get_chat_member_count(main_chat_id)} total")
 
     bot.reply_to(message, f"Printed all the stats, check the console")
     print("{date} DEV: {username} with id {id} called \"/stats\" in {chat_id}".format(date=datetime.now(), username=message.from_user.username, id=message.from_user.id, chat_id=message.chat.id))
