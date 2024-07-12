a4ad0f45aeb0acc1f0c7716c05c0733ca8f312f4
Nikita-LAPTOP <clowixdev@gmail.com>
2024-07-12 03:45:04 +0300

    added 'stats' command

--- commands/admin.py
+++ commands/admin.py
@@ -19,9 +19,34 @@ def clear_command(message: Message)-> None:
         message (Message): Object, that contains information of received message
     """
     all_users = gen_users(engine)
+    deleted_users = 0
     for user in all_users:
         if user.username == None:
+            deleted_users += 1
             print(f"DEV: deleted user {user.rr_name} - {user.crit_dmg}%")
             delete_user(user.id, engine)
 
+    bot.reply_to(message, f"Deleted {deleted_users} users, check the console")
+
     print("{date} DEV: {username} with id {id} called \"/clear\" in {chat_id}".format(date=datetime.now(), username=message.from_user.username, id=message.from_user.id, chat_id=message.chat.id))
+
+
+@bot.message_handler(commands=["stats"])
+@spam_checker
+@dev_required
+@chat_required
+def stats_command(message: Message)-> None:
+    """Handler that will provide work for stats command that will give me brief info about clan
+
+    Args:
+        message (Message): Object, that contains information of received message
+    """
+
+    all_users = gen_users(engine)
+    for id, user in enumerate(user, 1):
+        print(f"DEV: {id}) {user.username} - {user.rr_name} - {user.crit_dmg}%")
+
+    print(f"DEV: total {len(all_users)} users registered")
+
+    bot.reply_to(message, f"Printed all the stats, check the console")
+    print("{date} DEV: {username} with id {id} called \"/stats\" in {chat_id}".format(date=datetime.now(), username=message.from_user.username, id=message.from_user.id, chat_id=message.chat.id))
