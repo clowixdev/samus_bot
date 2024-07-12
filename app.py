422c64843e50331a7228b46cca68a6bc933b9d2f
Nikita-LAPTOP <clowixdev@gmail.com>
2024-07-12 03:44:14 +0300

    added api telegram exception handler

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from loader import bot
+from telebot import apihelper
 from time import sleep
 from datetime import datetime
 from requests.exceptions import ConnectionError, ReadTimeout
@@ -10,10 +11,11 @@ if __name__ == "__main__":
         try:
             print(f"Bot script has been successfully enabled at {datetime.now()}")
             bot.polling(non_stop=True, interval=0)
-        except KeyboardInterrupt:
-            print(f"Shutting down the bot at {datetime.now()}")
-            exit(0)
-        except (ConnectionError, ReadTimeout) as r_e:
-            print(datetime.now(), r_e)
-            sleep(5)
+        except apihelper.ApiTelegramException as tele_e:
+            print("EXCEPTION:", datetime.now(), tele_e)
+            sleep(4)
+            continue
+        except (ConnectionError, ReadTimeout) as req_e:
+            print("EXCEPTION:", datetime.now(), req_e)
+            sleep(4)
             continue
